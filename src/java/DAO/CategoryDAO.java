b15d9af2cdf51b678bad9c0339b5aaaae1e19001
Tuan Phong <Tuan Phong@DESKTOP-H9O8MOT>
2022-05-31 17:32:33 +0700

    create CategoryDAO

---
+++ src/java/DAO/CategoryDAO.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Category;
+
+/**
+ *
+ * @author Tuan Phong
+ */
+public class CategoryDAO extends DAO {
+    public ArrayList<Category> list() {
+    ArrayList<Category> categorys = new ArrayList<>();
+        try {
+            xSql = "SELECT * from Category";
+            ps = connection.prepareStatement(xSql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Category category = new Category();
+                category.setCid(rs.getInt("cid"));
+                category.setCname(rs.getString("name"));
+                categorys.add(category);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CategoryDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return categorys;
+    }
+}
