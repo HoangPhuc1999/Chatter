4e5d220d093dc00af1f40ff40141d019e7878e33
/npn/ <84844071+02468chonsp@users.noreply.github.com>
2022-06-03 03:52:23 +0700

    new controller, change some controllers directory.
    Add InsertCategoryController, UpdateCategoryController, change ListCategoryController into controller.admin.category.
    Change UpdateUserController into controller.admin.user.
    Completed those controllers.(?)

---
+++ src/java/controller/admin/category/UpdateController.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.admin.category;
+
+import DAO.CategoryDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Category;
+
+/**
+ *
+ * @author Tuan Phong
+ */
+public class UpdateController extends HttpServlet {
+   
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    } 
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        Category category = new Category(Integer.parseInt(request.getParameter("cid")),request.getParameter("cname"));
+        CategoryDAO categoryDAO = new CategoryDAO();
+        categoryDAO.update(category);
+        response.sendRedirect("../../../view/admin/ListCategory.jsp");
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
