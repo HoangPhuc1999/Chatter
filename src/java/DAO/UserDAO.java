33ca36c4ef762a105e4b3e63ba14b24be295ac65
khuat <khuat@Wlaptop>
2022-06-04 02:23:46 +0700

    fix sign up

--- src/java/DAO/UserDAO.java
+++ src/java/DAO/UserDAO.java
@@ -9,8 +9,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.User;
-import model.UserAccount;
+import model.*;
 
 /**
  * /**
@@ -79,6 +78,196 @@ public class UserDAO extends DAO {
             Logger.getLogger(UserDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
         return users;
+    }
+    
+    //author: an 
+    //Them user vao 4 bang User,user account, user address, user role
+    //state: insert xong bang users thi dung lai
+    public void singup(User newUser,UserAccount newUserAccount,UserAddress newUserAddress){
+        String query = "insert into users(firstname, lastname, phonenumber, email, gender, avatar)\n"
+                + "values(?,?,?,?,?,?)";
+        String idquery1 = "select SCOPE_IDENTITY() AS [SCOPE_IDENTITY]"; //ko dc 
+        
+        String idquery = "SELECT users_id \n" +
+                        "FROM users \n" +
+                        "WHERE users_id = (SELECT MAX(users_id) FROM Users)";
+        
+        String query2 = "insert into users_role(users_id,user_role)\n"
+                + "values(?,?)";
+        String query3 = "insert into users_account(users_id,username,password)\n"
+                + "values(?,?,?)";
+        String query4 = "insert into users_address(users_id,home_address,district,city)\n"
+                + "values(?,?,?,?)";
+        try {
+            int users_id = 99999;
+            //them vao users
+            ps = con.prepareStatement(query);
+            ps.setString(1, newUser.getFirstname());
+            ps.setString(2, newUser.getLastname());
+            ps.setString(3, newUser.getPhonenumber());
+            ps.setString(4, newUser.getEmail());
+            ps.setString(5, newUser.getGender());
+            ps.setString(6, newUser.getAvatar()); 
+            ps.executeUpdate();
+            ps.clearParameters();
+            
+            //LAY USERS_ID
+//            rs = ps.getGeneratedKeys();
+//            if(rs.next()){
+//                users_id = rs.getInt("ID");
+//            }
+            ps = con.prepareStatement(idquery);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                users_id = rs.getInt(1);
+            } 
+            ps.clearParameters();
+            
+            
+            //them vao user_role
+            ps = con.prepareStatement(query2);
+            ps.setInt(1,users_id);
+            ps.setString(2,"user");
+            ps.executeUpdate();
+            ps.clearParameters();
+             
+            //them vao user_account
+            ps = con.prepareStatement(query3);
+            ps.setInt(1,users_id);
+            ps.setString(2,newUserAccount.getUsername());
+            ps.setString(3,newUserAccount.getPassword());
+            ps.executeUpdate();
+            ps.clearParameters();
+            
+            //them vao user_address
+            ps = con.prepareStatement(query4);
+            ps.setInt(1,users_id);
+            ps.setString(2,newUserAddress.getHome_address());
+            ps.setString(3,newUserAddress.getDistrict());
+            ps.setString(4,newUserAddress.getCity());
+            ps.executeUpdate();
+            ps.clearParameters();
+            
+            ps.close();
+            rs.close();
+            
+        } catch (Exception e) {
+        }
+    }
+    
+    //author: an 
+    //Lay users_id moi nhat sau khi insert user
+    //old
+    public int getUserIDold() throws SQLException {
+        rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                int users_id = rs.getInt("ID");
+                return users_id;
+            }
+        
+        return 0;
+    }
+    
+    
+    
+    //author: an 
+    //Lay users_id moi nhat sau khi insert user = max
+    public int getUserID() throws SQLException {
+        String query = "SELECT users_id \n" +
+                        "FROM users \n" +
+                        "WHERE users_id = (SELECT MAX(users_id) FROM Users)";
+        try {
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+        }
+        return 99999;
+    }
+    
+    
+    
+    
+    
+    //author: an 
+    //Kiem tra username da co chua
+    public UserAccount checkAccountExist(String user) {
+        String query = "select * from users_account\n"
+                + "where Username = ?\n";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setString(1, user);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new UserAccount(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+    
+    //reset mat khau table UserAccount 
+    //Created by An at 27/5
+    public void resetPassword(String username) {
+        String query = "Update users_account\n"
+                + "Set Password = !@#$%"
+                + "where Username = ?\n"
+                + "";
+        try {
+            ps = con.prepareStatement(query);
+            ps.setString(1, username);
+            ps.executeUpdate(query);
+        } catch (Exception e) {
+
+        }
+    }
+    
+    //lay tat ca user trong bang users 
+    public ArrayList<User> getAllUsers() {
+        ArrayList<User> allUserList = new ArrayList<>();
+        String query = "select * from users";
+        try {
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                allUserList.add(new User(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7)));
+            }
+        } catch (Exception e) {
+            
+        }
+        return allUserList;
+    }
+    
+    
+    public static void main(String[] args) throws SQLException {
+        UserDAO dao = new UserDAO();
+        UserAccount a = new UserAccount();
+        a = dao.checkAccountExist("user1");
+        
+        //check sign up
+//        UserAccount newAcc = new UserAccount("x","x");  
+//        UserAddress newAccAddress= new UserAddress("x","x","x");
+//        User newAccUser = new User("ggqeq","gqw","user3@fpt.edu.vn","0675978234","1","images/boy.jpg");
+//        
+//        dao.singup(newAccUser,newAcc,newAccAddress); //them user
+//        
+        
+        ArrayList<User> b = dao.getAllUsers();
+        
+        int x = dao.getUserID();
+        
+        
+        System.out.println(x);
     }
 
 }
