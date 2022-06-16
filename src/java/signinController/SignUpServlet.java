168cd118e2abe3d48003609ef525d2be99e31455
KQuangAn <khuatquangan88@gmail.com>
2022-06-16 22:32:58 +0700

    16/6 update index, menu fix sign up

--- src/java/signinController/SignUpServlet.java
+++ src/java/signinController/SignUpServlet.java
@@ -9,8 +9,14 @@ import DAO.*;
 import model.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -20,6 +26,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
@@ -38,18 +45,15 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 
 public class SignUpServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-    }
-
+    
+       
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         String user = request.getParameter("username");
         String pass = request.getParameter("password");
-        String re_pass = request.getParameter("repassword");
+        //String re_pass = request.getParameter("repassword");
         String home_address = request.getParameter("home_address");
         String district = request.getParameter("district");
         String city = request.getParameter("city");
@@ -58,26 +62,110 @@ public class SignUpServlet extends HttpServlet {
         String phone = request.getParameter("phonenumber");
         String email = request.getParameter("email");
         String gender = request.getParameter("gender");
-        String avapath="avatar\\male.jpg";
+        String avapath="";
         
-        //upload image to project 's avatar under web folder and save path to avapath
-        try{
-            Part part = request.getPart("file");
-            part.write(extractFileName(part));
-            avapath=("avatar\\"+extractFileName(part)).toString();
+//        String saveFolder = System.getProperty("user.dir")+ File.separator +"web"+File.separator +"avatar";
+//        //Part part = request.getPart("avatar");
+//                
+//        File fileUploadDirectory = new File(saveFolder + part.getSubmittedFileName());        
+//        if (!fileUploadDirectory.exists()) {
+//            fileUploadDirectory.mkdirs();
+//        }
 
+//        Part filePart = request.getPart("avatar"); 
+//            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // MSIE fix.
+//            InputStream fileContent = filePart.getInputStream();
+//            avapath = "avatar"+File.separator + fileName;
+//            
+//        if (ServletFileUpload.isMultipartContent(request)) {
+//
+//            DiskFileItemFactory factory = new DiskFileItemFactory();
+//            factory.setRepository(new File(System.getProperty("user.dir")));
+//            ServletFileUpload upload = new ServletFileUpload(factory);
+//            String uploadPath = System.getProperty("user.dir")+ File.separator +"web"+File.separator +"avatar";
+//            File uploadDir = new File(uploadPath);
+//            if (!uploadDir.exists()) {
+//                uploadDir.mkdir();
+//            }  
+//            try{
+//            List<FileItem> formItems = upload.parseRequest(request);
+//            if (formItems != null && formItems.size() > 0) {
+//                for (FileItem item : formItems) {
+//                    if (!item.isFormField()) {
+//                        String fileName = new File(item.getName()).getName();
+//                        String filePath = uploadPath + File.separator + fileName;
+//                        File storeFile = new File(filePath);
+//                        item.write(storeFile);
+//                    }
+//            }
+//            }
+//            
+//            }
+//            catch(Exception e){
+//                
+//            }
+//        }
+
+//        
+//        String savePath = saveFolder + File.separator + fileName;
+//        part.write(savePath + File.separator);
+//        
+// 
+//        //upload image to project 's avatar under web folder and save path to avapath
+//        try{
+//            Part part = request.getPart("file");
+//            part.write(extractFileName(part));
+//            avapath=("avatar\\"+extractFileName(part)).toString();
+//
+//            }
+//            catch (Exception e) {
+//                System.out.println(e.getClass());
+//                
+//            }
+//              
+
+        // Create path components to save the file
+        final String path = "avatar";
+        final Part filePart = request.getPart("avatar");
+        final String fileName = getFileName(filePart);
+        avapath = "avatar/" + fileName;//path saveto database 
+        
+        OutputStream out = null;
+        InputStream filecontent = null;
+        final PrintWriter writer = response.getWriter();
+
+        try {
+            out = new FileOutputStream(new File(path + File.separator + fileName));
+            filecontent = filePart.getInputStream();
+
+            int read = 0;
+            final byte[] bytes = new byte[1024];
+
+            while ((read = filecontent.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
             }
-            catch (Exception e) {
-                System.out.println(e.getClass());
-                
+            
+            
+        } catch (FileNotFoundException fne) {
+            writer.println("You either did not specify a file to upload or are "
+                    + "trying to upload a file to a protected or nonexistent "
+                    + "location.");
+            writer.println("<br/> ERROR: " + fne.getMessage());
+
+            if(gender.equals(1)){
+                avapath="avatar/male.jpg";
             }
-              
+            else{
+                avapath="avatar/female.jpg";
+            }
+        }
+
            
 
         try{
             if(!user.matches("[a-zA-Z0-9 ]*")) throw new IOException("username is invalid");
             if(!pass.matches("[a-zA-Z0-9]*")) throw new IOException("password is invalid");
-            if(!pass.matches(re_pass)) throw new IOException("password does not match");
+            //if(!pass.matches(re_pass)) throw new IOException("password does not match");
             if(!phone.matches("[0-9]+")) throw new IOException("phone number is invalid");
             
             UserDAO dao = new UserDAO();
@@ -91,10 +179,7 @@ public class SignUpServlet extends HttpServlet {
                 
                 
                 request.setAttribute("message", "Sign up Success!");
-                PrintWriter out = response.getWriter(); 
-                out.println("<script type=\"text/javascript\">"); 
-                out.println("alert('Sign up Success!');"); 
-                out.println("</script>");
+
                 
                 request.setAttribute("avapath", avapath);
                 request.setAttribute("user", user);
@@ -145,6 +230,16 @@ public class SignUpServlet extends HttpServlet {
     }
     return "";
   }
+    private String getFileName(final Part part) {
+    final String partHeader = part.getHeader("content-disposition");
+    for (String content : part.getHeader("content-disposition").split(";")) {
+        if (content.trim().startsWith("filename")) {
+            return content.substring(
+                    content.indexOf('=') + 1).trim().replace("\"", "");
+        }
+    }
+    return null;
+}
     
    //upload image to folder 
   public File getFolderUpload() {
