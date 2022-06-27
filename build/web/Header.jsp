bb74845ebb491d1d54607b92e326a1a23bd24321
Hoang Phuc <abchoangphuc@gmail.com>
2022-06-27 22:00:48 +0700

    fix header

--- build/web/Header.jsp
+++ build/web/Header.jsp
@@ -39,7 +39,7 @@
                         <a class="nav-link" href="Chatting.jsp">Chatting</a>
                     </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="Review.jsp">Review</a>
+                        <a class="nav-link" href="getreview">Review</a>
                     </li>
                 </ul>
                 <div class="user_option">
--- src/java/reviewController/GetReviewServlet.java
+++ src/java/reviewController/GetReviewServlet.java
@@ -5,12 +5,17 @@
  */
 package reviewController;
 
+import DAO.ReviewDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Review;
 
 /**
  *
@@ -18,6 +23,19 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class GetReviewServlet extends HttpServlet {
 
-    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter pr = response.getWriter();
+        ReviewDAO dao = new ReviewDAO();
+        List<Review> list = new ArrayList<>();
+        list = dao.getAllReviews();
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getRating());
+        }
+        request.setAttribute("reviewlist", list);
+        request.getRequestDispatcher("Review.jsp").forward(request, response);
+    }
 
 }
