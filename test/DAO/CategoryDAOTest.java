f4e0fa46a39c46de81493eaf4f9f33788c57a7c6
KQuangAn <khuatquangan88@gmail.com>
2022-07-01 10:32:10 +0700

    Merge origin/master into an286
    Conflicts:
    	build/web/WEB-INF/classes/DAO/ProductDAO.class
    	web/view/admin/AddUser.jsp
    	web/view/admin/Sidebar.jsp
    	web/view/admin/UserDetails.jsp

---
+++ test/DAO/CategoryDAOTest.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.ArrayList;
+import model.Category;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author khuat
+ */
+public class CategoryDAOTest {
+    
+    public CategoryDAOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testListAllCategory() {
+        System.out.println("listAllCategory");
+        CategoryDAO instance = new CategoryDAO();
+        ArrayList<Category> expResult = null;
+        ArrayList<Category> result = instance.listAllCategory();
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testInsert() {
+        System.out.println("insert");
+        Category category = null;
+        CategoryDAO instance = new CategoryDAO();
+        instance.insert(category);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testUpdate() {
+        System.out.println("update");
+        Category category = null;
+        CategoryDAO instance = new CategoryDAO();
+        instance.update(category);
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testDelete() {
+        System.out.println("delete");
+        int categoryId = 0;
+        CategoryDAO instance = new CategoryDAO();
+        instance.delete(categoryId);
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ test/DAO/ProductDAOTest.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.util.List;
+import model.Category;
+import model.Product;
+import model.ProductDetails;
+import model.ProductImage;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author khuat
+ */
+public class ProductDAOTest {
+    
+    public ProductDAOTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getProductById method, of class ProductDAO.
+     */
+    @Test
+    public void testGetProductById() {
+        System.out.println("getProductById");
+        String id = "";
+        ProductDAO instance = new ProductDAO();
+        Product expResult = null;
+        Product result = instance.getProductById(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAllProduct method, of class ProductDAO.
+     */
+    @Test
+    public void testGetAllProduct() {
+        System.out.println("getAllProduct");
+        ProductDAO instance = new ProductDAO();
+        List<Product> expResult = null;
+        List<Product> result = instance.getAllProduct();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAllProductWithCategory method, of class ProductDAO.
+     */
+    @Test
+    public void testGetAllProductWithCategory() {
+        System.out.println("getAllProductWithCategory");
+        ProductDAO instance = new ProductDAO();
+        List<Product> expResult = null;
+        List<Product> result = instance.getAllProductWithCategory();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getProductImageById method, of class ProductDAO.
+     */
+    @Test
+    public void testGetProductImageById() {
+        System.out.println("getProductImageById");
+        String id = "";
+        ProductDAO instance = new ProductDAO();
+        ProductImage expResult = null;
+        ProductImage result = instance.getProductImageById(id);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAllCategory method, of class ProductDAO.
+     */
+    @Test
+    public void testGetAllCategory() {
+        System.out.println("getAllCategory");
+        ProductDAO instance = new ProductDAO();
+        List<Category> expResult = null;
+        List<Category> result = instance.getAllCategory();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getProductByCID method, of class ProductDAO.
+     */
+    @Test
+    public void testGetProductByCID() {
+        System.out.println("getProductByCID");
+        String cid = "";
+        ProductDAO instance = new ProductDAO();
+        List<Product> expResult = null;
+        List<Product> result = instance.getProductByCID(cid);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of couponCheck method, of class ProductDAO.
+     */
+    @Test
+    public void testCouponCheck() {
+        System.out.println("couponCheck");
+        String code = "";
+        ProductDAO instance = new ProductDAO();
+        int expResult = 0;
+        int result = instance.couponCheck(code);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAllProductDetailses method, of class ProductDAO.
+     */
+    @Test
+    public void testGetAllProductDetailses() {
+        System.out.println("getAllProductDetailses");
+        ProductDAO instance = new ProductDAO();
+        List<ProductDetails> expResult = null;
+        List<ProductDetails> result = instance.getAllProductDetailses();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class ProductDAO.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        ProductDAO.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
