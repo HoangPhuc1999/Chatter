4fc3db8fd320a639a10fc99d5cbbf81eb5eb242b
Hoang Phuc <abchoangphuc@gmail.com>
2022-07-15 13:03:14 +0700

    add print exception to log in review Dao and orderdetaildao

--- src/java/DAO/OrderDetailDAO.java
+++ src/java/DAO/OrderDetailDAO.java
@@ -5,10 +5,45 @@
  */
 package DAO;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.OrderDetail;
+
 /**
  *
  * @author Hoang Phuc
  */
-public class OrderDetailDAO {
+public class OrderDetailDAO extends DAO {
+    
+    public List<OrderDetail> getAllOrderDetail() {
+        List<OrderDetail> list = new ArrayList<>();
+        xSql = "select orders_details.order_id, orders_details.order_product_id,orders_details.order_amount,"
+                +"orders_details.order_date,products.product_name,products_image.product_image_path"
+                +"from orders_details"
+                +"join products on orders_details.order_product_id = products.product_id"
+                +"join products_image on orders_details.order_product_id = products_image.product_id";
+
+        try {
+            ps = con.prepareStatement(xSql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                OrderDetail x = new OrderDetail();
+                x.setOrderId(rs.getInt("order_id"));
+                x.setOrderProductId(rs.getInt("order_product_id"));
+                x.setOrderAmount(rs.getInt("order_amount"));
+                x.setOrderDate(rs.getDate("order_date"));
+                x.setProductName(rs.getString("product_name"));
+                x.setProductImagePath(rs.getString("product_image_path"));
+                list.add(x);
+            }
+            rs.close();
+            ps.close();
+        } catch (Exception e) {
+            System.out.println("Get Order Detail Fail");
+            System.out.println(e.toString());
+        }
+        return (list);
+
+    }
     
 }
